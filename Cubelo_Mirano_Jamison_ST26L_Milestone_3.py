31eb2534107c99b8e7ea33d825f63fadaad64338
christelmirano <ccmirano@up.edu.ph>
2025-05-21 02:11:44 +0800

    feat: working menu

--- Cubelo_Mirano_Jamison_ST26L_Milestone_3.py
+++ Cubelo_Mirano_Jamison_ST26L_Milestone_3.py
@@ -74,7 +74,7 @@ def display_menu():
           exit_system()
           break
       else:
-          print("Invalid choice. Please try again.")
+          print("Invalid choice. Please try again.\n")
 
 def exit_system():
   print("exiting program...\n")
@@ -84,16 +84,18 @@ def view_all_orgs():
   while True:
     print("\n"+"-"*50)
     print("ALL ORGANIZATIONS".center(50))
+    print("-"*50)
     print("\n TABLE \n") # PLACEHOLDER: display the organizations table here
+    print("\n"+"-"*50)
 
     orgId = input("(Press ENTER to go back to home)\nSelect an organization to view:  ")
     if orgId==None:
-      print("Organization ID cannot be empty!")
+      print("Organization ID cannot be empty!\n")
       continue
     elif orgId=="":
       return
     # elif orgId != PLACEHOLDER: palagyan ng validation if wala sa choices yung sinagot nila
-    # print("Organization not found.")
+    # print("Organization not found.\n")
     # continue
     else: 
       display_view_an_org_menu(orgId)
@@ -104,7 +106,7 @@ def display_view_an_org_menu(orgId):
     print("[1] View committees")
     print("[2] View members")
     print("[3] View Fees")
-    print("[0] Back")
+    print("[0] Back to Home")
 
     choice = input("Select a choice:  ")
 
@@ -117,7 +119,7 @@ def display_view_an_org_menu(orgId):
     elif choice == "0":
       break
     else:
-      print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.\n")
   
 
 
@@ -136,14 +138,14 @@ def view_org_committees(orgId):
     elif choice == '2':
       commitee = input("Enter a committee: ")
       if commitee == None or commitee == "":
-        print("Committee cannot be empty!")
+        print("Committee cannot be empty!\n")
         continue
       else:
         view_committee_members(orgId, commitee)
     elif choice == '0':
       break
     else:
-      print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.\n")
 
 def view_all_committees(orgId):
   print("\nview_all_committees: ", orgId, "\n") # PLACEHOLDER: display all committees here
@@ -171,8 +173,11 @@ def view_org_members(orgId):
     elif choice == '2':
       memId = input("Enter student id: ")
       if memId == None or memId == "":
-        print("Student ID cannot be empty!")
+        print("Student ID cannot be empty!\n")
         continue
+      # elif orgId != PLACEHOLDER: palagyan ng validation if wala sa database yung sinagot nila
+      # print("Student ID not found.\n")
+      # continue
       else:
         show_a_member(memId,orgId)
     elif choice == '3':
@@ -184,7 +189,7 @@ def view_org_members(orgId):
     elif choice == '0':
       return
     else:
-      print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.\n")
 
 def show_all_members(orgId):
   print("\nshow_all_members: ", orgId, "\n") # PLACEHOLDER: display all members here
@@ -221,7 +226,7 @@ def view_org_fees(orgId):
     elif choice == '2':
       feeID = input("Enter fee id: ")
       if feeID == None or feeID == "":
-        print("Fee ID cannot be empty!")
+        print("Fee ID cannot be empty!\n")
         continue
       else:
         show_a_fee(feeID,orgId)
@@ -232,7 +237,7 @@ def view_org_fees(orgId):
     elif choice == '0':
       return
     else:
-      print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.\n")
 
 def show_all_fees(orgId):
   print("\nshow_all_fees: ", orgId, "\n") # PLACEHOLDER: display all fee here
@@ -266,16 +271,18 @@ def update_database():
   while True:
     print("\n"+"-"*50)
     print("UPDATE AN ORGANIZATION".center(50))
+    print("-"*50)
     print("\n TABLE \n") # PLACEHOLDER: display the organizations table here
+    print("\n"+"-"*50)
 
     orgId = input("(Press ENTER to go back to home)\nSelect an organization to update:  ")
     if orgId==None:
-      print("Organization ID cannot be empty!")
+      print("Organization ID cannot be empty!\n")
       continue
     elif orgId=="":
       return
     # elif orgId != PLACEHOLDER: palagyan ng validation if wala sa choices yung sinagot nila
-    # print("Organization not found.")
+    # print("Organization not found.\n")
     # continue
     else: 
       display_update_an_org_menu(orgId)
@@ -286,7 +293,7 @@ def display_update_an_org_menu(orgId):
     print("[2] Update a member")
     print("[3] Update a fee")
     print("[4] Update a member's status on a fee")
-    print("[0] Back")
+    print("[0] Back to Home")
 
     choice = input("Select a choice: ")
 
@@ -301,11 +308,11 @@ def display_update_an_org_menu(orgId):
     elif choice == '0':
       return
     else:
-      print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.\n")
 
 def update_a_committee(orgId):
   print("\nUPDATE A COMMITTEE\n")
-  print("\nUPDATE A COMMITTEE\n") # PLACEHOLDER : show committees of the org
+  print("TABLE\n") # PLACEHOLDER : show committees of the org
   while True: 
     print("[1] Delete a committee")
     print("[2] Rename a committee")
@@ -316,10 +323,10 @@ def update_a_committee(orgId):
     if choice == '1':
       committee = input("Enter committee to delete: ")
       if committee==None or committee=="":
-        print("please enter name of committee.")
+        print("please enter name of committee.\n")
         continue
       # elif committee != PLACEHOLDER: palagyan ng validation if wala sa choices yung sinagot nila
-      # print("committee not found.")
+      # print("committee not found.\n")
       # continue
       else:
         delete_a_committee(orgId, committee)
@@ -329,7 +336,7 @@ def update_a_committee(orgId):
         print("please enter name of committee.")
         continue
       # elif committee != PLACEHOLDER: palagyan ng validation if wala sa choices yung sinagot nila
-      # print("committee not found.")
+      # print("committee not found.\n")
       # continue
       else:
         newName = input("Enter new committee name: ")
@@ -337,7 +344,7 @@ def update_a_committee(orgId):
     elif choice == '0':
       return
     else:
-      print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.\n")
 
 def delete_a_committee(orgId, committee):
   print("\ndelete_a_committee\n") # PLACEHOLDER
@@ -357,47 +364,47 @@ def update_a_member(orgId):
     print("[5] Update committee")
     print("[0] Back")
 
-    memId = input("Enter student id: ")
     choice = input("Select a choice: ")
+    memId = input("Enter student id: ")
 
     if memId==None or memId=="":
-      print("Student id cannot be empty!")
+      print("Student id cannot be empty!\n")
       continue
 
     if choice == '1':
       update = input("Enter NEW status: ")
       if update==None or update=="":
-        print("Nothing updated!")
+        print("Nothing updated!\n")
         continue
       update_active_status(orgId, memId, update)
     elif choice == '2':
       update = input("Enter NEW role: ")
       if update==None or update=="":
-        print("Nothing updated!")
+        print("Nothing updated!\n")
         continue
       update_role(orgId, memId, update)
     elif choice == '3':
       update = input("Enter NEW degree program: ")
       if update==None or update=="":
-        print("Nothing updated!")
+        print("Nothing updated!\n")
         continue
       update_degree_program(orgId, memId, update)
     elif choice == '4':
       update = input("Enter NEW gender: ")
       if update==None or update=="":
-        print("Nothing updated!")
+        print("Nothing updated!\n")
         continue
       update_gender(orgId, memId, update)
     elif choice == '5':
       update = input("Enter NEW committee: ")
       if update==None or update=="":
-        print("Nothing updated!")
+        print("Nothing updated!\n")
         continue
       update_member_committee(orgId, memId, update)
     elif choice == '0':
       return
     else:
-      print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.\n")
 
 def update_active_status(orgId, memId, update):
   print("\nupdate_active_status\n") # PLACEHOLDER
@@ -428,35 +435,39 @@ def update_a_fee(orgId):
     choice = input("Select a choice: ")
 
     if feeId==None or feeId=="":
-      print("Fee id cannot be empty!")
+      print("Fee id cannot be empty!\n")
       continue
 
     if choice == '1':
-      update = input("Enter NEW due date: ")
-      if update==None or update=="":
-        print("Nothing updated!")
-        continue
-      update_fee_due(orgId, feeId, update)
+      update_fee_due(orgId)
     elif choice == '2':
       update = input("Enter NEW purpose: ")
       if update==None or update=="":
-        print("Nothing updated!")
+        print("Nothing updated!\n")
         continue
       update_fee_purpose(orgId, feeId, update)
     elif choice == '3':
       update = input("Enter NEW amount: ")
       if update==None or update=="":
-        print("Nothing updated!")
+        print("Nothing updated!\n")
         continue
       update_fee_amount(orgId, feeId, update)
     elif choice == '0':
       return
     else:
-      print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.\n")
 
 def update_fee_due(orgId, feeId, update):
   print("\nupdate_fee_due\n") # PLACEHOLDER
-
+  while True:
+    update = input("Enter NEW due date: ")
+    if update==None or update=="":
+      print("Nothing updated!\n")
+      continue
+    else: 
+      # PLACEHOLDER place code here
+      break
+    
 def update_fee_purpose(orgId, feeId, update):
   print("\nupdate_fee_purpose\n") # PLACEHOLDER
 
@@ -475,11 +486,11 @@ def update_member_fee_status(orgId):
     choice = input("Select a choice: ")
 
     if feeId==None or feeId=="":
-      print("Fee id cannot be empty!")
+      print("Fee id cannot be \n")
       continue
       
     if memId==None or memId=="":
-      print("Student id cannot be empty!")
+      print("Student id cannot be empty!\n")
       continue
 
     if choice == '1':
@@ -490,30 +501,212 @@ def update_member_fee_status(orgId):
 
       paid = input("Select a choice: ")
       if paid==None or paid=="":
-        print("choice cannot be empty!")
+        print("choice cannot be empty!\n")
         continue
       date = input("Enter payment date (YYYY-MM-DD): ")
       if date==None or date=="":
-        print("date cannot be empty!")
+        print("date cannot be empty!\n")
         continue
       update_member_payment_status(orgId, feeId, memId, date)
 
     elif choice == '0':
       return
     else:
-      print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.\n")
 
 def update_member_payment_status(orgId, feeId, memId, date):
   print("\nupdate_member_payment_status\n") # PLACEHOLDER
 
 ########################### INSERT ################################
 def insert_database():
-  print("\ninsert_database\n") # PLACEHOLDER
+  while True:
+    print("\n"+"-"*50)
+    print("INSERT MODIFICATIONS".center(50))
+    print("-"*50)
+
+    print("")
+    print("[1] Insert a new member")
+    print("[2] Add a new organization")
+    print("[3] Create a new fee")
+    print("[0] Back to Home")
+
+    choice = input("Select a choice:  ")
+
+    if choice == "1":
+      insert_new_member()
+    elif choice == "2":
+      insert_new_organization()
+    elif choice == "3":
+      insert_new_fee()
+    elif choice == "0":
+      break
+    else:
+      print("Invalid choice. Please try again.\n")
+
+def insert_new_member():
+  while True: 
+    orgId = input("Enter organization ID: ")
+    if orgId==None or orgId=="":
+        print("Organization ID cannot be empty!\n")
+        continue
+      # elif orgId != PLACEHOLDER: palagyan ng validation if wala sa choices yung sinagot nila
+      # print("Organization not found.\n")
+      # continue
+    print("")
+    fname = input("Enter First Name: ")
+    lname = input("Enter Last Name: ")
+    degprog = input("Enter Degree Program: ")
+    batch = input("Enter Batch: ")
+    committee = input("Enter Committee: ")
+    role = input("Enter Role: ")
+    status = input("Enter Status: ")
+    gender = input("Enter Gender: ")
+    semester = input("Enter Semester Joined: ")
+
+    if fname=="" or lname=="" or degprog=="" or batch=="" or committee=="" or role=="" or status=="" or gender=="" or semester=="":
+      print("You missed a field!\n")
+      continue
+    # PLACEHOLDER : add proper validation for the input para sa kung ano lang pwede ilagay
+    # elif  
+    #   print("Invalid input. Must be either President/Vice President")
+    #   continue
+    else:
+      newMemId = adding_member(orgId, fname, lname, degprog, batch, committee, role, status, gender, semester)
+      display_new_member(newMemId)
+      break
+
+def adding_member(orgId, fname, lname, degprog, batch, committee, role, status, gender, semester):
+  print("\ninsert_new_member\n") # PLACEHOLDER
+  newMemId = "dummyId" # PLACEHOLDER
+  return newMemId
+
+def display_new_member(newMemId):
+  print("\ndisplay_new_member\n") # PLACEHOLDER
+
+def insert_new_organization():
+  while True:
+    orgName = input("Enter the name of NEW organization: ")
+    orgType = input("Enter the type of organization: ")
+
+    if orgName=="" or orgType=="":
+      print("You missed a field!\n")
+      continue
+    # PLACEHOLDER : add proper validation for the input para sa kung ano lang pwede ilagay
+    # elif  
+    #   print("Invalid input. Must be either President/Vice President")
+    #   continue
+    else: 
+      orgId = adding_organization(orgName, orgType)
+      display_new_organization(orgId)
+      break
+
+def adding_organization(orgName, orgType):
+  print("\nadding_organization\n") # PLACEHOLDER
+  orgId = "dummyId"
+  return orgId
+
+def display_new_organization(orgId):
+  print("\ndisplay_new_organization\n") # PLACEHOLDER
+
+def insert_new_fee():
+  while True: 
+    orgId = input("Enter organization ID: ")
+    if orgId==None or orgId=="":
+      print("Organization ID cannot be empty!\n")
+      continue
+    # elif orgId != PLACEHOLDER: palagyan ng validation if wala sa choices yung sinagot nila
+    # print("Organization not found.\n")
+    # continue
+
+    purpose = input("Enter Purpose of Fee: ")
+    amount = input("Enter Amount to Pay: ")
+    duedate = input("Enter Due Date: ")
+
+    if purpose=="" or amount=="" or duedate=="":
+      print("You missed a field!\n")
+      continue
+    # PLACEHOLDER : add proper validation for the input para sa kung ano lang pwede ilagay
+    # elif  
+    #   print("Invalid input. Must be either President/Vice President")
+    #   continue
+    else:
+      newFeeId = adding_fee(orgId, purpose, amount, duedate)
+      display_new_fee(newFeeId)
+      break
+
+def adding_fee(orgId, purpose, amount, duedate):
+  print("\nadding_fee\n") # PLACEHOLDER
+  feeId = "dummyId"
+  return feeId
+
+def display_new_fee(newFeeId):
+  print("\ndisplay_new_fee\n") # PLACEHOLDER
+
+
+
 
 ########################### DELETE ################################
 def delete_database():
-  print("\ndelete_database\n") # PLACEHOLDER
+  print("\nDELETE FROM DATABASE\n")
+  while True: 
+    print("[1] Delete an organization")
+    print("[2] Delete an member")
+    print("[0] Back to Home")
 
+    choice = input("Select a choice: ")
+
+    if choice == '1':
+      delete_organization()
+    elif choice == '2':
+      delete_member()
+    elif choice == '0':
+      break
+    else:
+      print("Invalid choice. Please try again.\n")
+
+def delete_organization():
+  while True:
+    orgId = input("Enter Organization ID: ")
+    if orgId==None or orgId=="":
+      print("Organization ID cannot be empty!\n")
+      continue
+
+    # elif orgId != PLACEHOLDER: palagyan ng validation if wala sa choices yung sinagot nila
+    # print("Organization not found.\n")
+    # continue
+    
+    else: 
+      deleting_organization(orgId)
+      break
+
+def deleting_organization(orgId):
+  # PLACEHOLDER: delete code here
+  print("\ndeleting_organization\n") # PLACEHOLDER: show the updated table of organization
+
+def delete_member():
+  while True:
+    orgId = input("Enter Organization ID: ")
+    if orgId==None or orgId=="":
+      print("Organizatoin ID cannot be empty!\n")
+      continue
+    # elif orgId != PLACEHOLDER: palagyan ng validation if wala sa database yung sinagot nila
+    # print("Student ID not found.\n")
+    # continue
+    else:
+      memId = input("Enter Student ID: ")
+      if memId==None or memId=="":
+        print("Student ID cannot be empty!\n")
+        continue
+      # elif orgId != PLACEHOLDER: palagyan ng validation if wala sa database yung sinagot nila
+      # print("Student ID not found.\n")
+      # continue
+      else: 
+        deleting_member(orgId, memId)
+        break
+
+def deleting_member(orgId, memId):
+  # PLACEHOLDER: delete code here
+  print("\ndeleting_member\n") # PLACEHOLDER: show the updated table of members
 
 
 ########################### FUNCTION CALL ################################
